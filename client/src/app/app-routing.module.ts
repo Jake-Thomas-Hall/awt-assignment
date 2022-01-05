db1d67bb5aa9c04d93636d82732a6db667b01921
Jake Hall <jakethomashall@gmail.com>
2022-01-05 11:31:50 +0000

    Start on admin section implementation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/admin/login/login.component';
 import { HomeComponent } from './components/pages/home/home.component';
 
-const routes: Routes = [{
-  path: '', component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'admin', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
